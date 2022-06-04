6ad9e30db8e400520d430ddf1cd27a34efe17131
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-06-03 23:23:25 -0400

    added the useState hook to the NameForm component

--- src/Components/NameForm.js
+++ src/Components/NameForm.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function NameForm(){
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    // const [countryOrigin, setCountryOrigin] = useState('')
+    // const [regionOrigin, setRegionOrigin] = useState('')
+    // const [subRegionOrigin, setSubRegionOrigin] = useState('')
+
+
+    const submitForm = (event) => {
+        event.preventDefault()
+        
+    }
 
 
     return(
@@ -9,7 +21,12 @@ function NameForm(){
             <form>
                 <fieldset>
                     <div style={{textAlign: 'center'}}>
-                        <label> Type In Your Last Name 
+                        <label> Enter Your First Name 
+                            <br /><br />
+                            <input type="text" placeholder='First Name'/>
+                        </label>
+                        <br /><br />
+                        <label> Enter Your Last Name 
                             <br /><br />
                             <input type="text" placeholder='Last Name'/>
                         </label>
@@ -21,6 +38,33 @@ function NameForm(){
                 </div>
             </form>
             <h2 style={{textAlign: 'center'}}>Results</h2>
+            <table>
+                <tr>
+                    <td>First Name:</td>
+                    <td>{firstName}</td>
+                </tr>
+                <br />
+                <tr>
+                    <td>Last Name:</td>
+                    <td>{lastName}</td>
+                </tr>
+                <br />
+                <tr>
+                    <td>Country Origin:</td>
+                    {/* <td>{countryOrigin}</td> */}
+                </tr>
+                <br />
+                <tr>
+                    <td>Region Origin:</td>
+                    {/* <td>{regionOrigin}</td> */}
+                </tr>
+                <br />
+                <tr>
+                    <td>Sub Region Origin:</td>
+                    {/* <td>{subRegionOrigin}</td> */}
+                </tr>
+                <br />
+            </table>
         </div>
     )
 
